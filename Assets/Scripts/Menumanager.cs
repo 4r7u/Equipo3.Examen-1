5eb7b78020fb1593e18e4832d42974bd4e64965a
4r7u <artupikachu@gmail.com>
2023-09-27 22:29:54 -0600

    Comit UI arturo 1

---
+++ Assets/Scripts/Menumanager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menumanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+
+    
+}
