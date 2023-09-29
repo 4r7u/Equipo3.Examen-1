bf01dd0053bdd872946c67dfa33a9cf8e6aef6da
4r7u <artupikachu@gmail.com>
2023-09-28 21:44:18 -0600

    Comit Pausa y menu
    Menu y pausa

---
+++ Assets/Scripts/Pausa.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausa : MonoBehaviour
+{
+    private bool gameIsPaused;
+    [SerializeField] private GameObject pauseMenu;
+    private void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+    void PauseGame(){
+            if (gameIsPaused){
+                pauseMenu.SetActive(true);
+            }else{
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+            }
+        }
+     public void Resume()
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+            Debug.Log("Resume");
+        }
+        public void Quit()
+        {
+            SceneManager.LoadScene(0);
+        }
+}
