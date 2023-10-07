c1ad95ef3156520389259c8943b343cdf4f7fa42
DanCasber <daniela.castillo@uniat.edu.mx>
2023-10-07 00:28:40 -0600

    load win scene

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
@@ -8,6 +9,8 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
 
     [SerializeField] private Text scoreText;
+
+    private GameObject[] collectables;
 
     int score = 0;
 
@@ -19,6 +22,7 @@ public class ScoreManager : MonoBehaviour
     void Start()
     {
         scoreText.text = score.ToString() + " POINTS";
+        collectables = GameObject.FindGameObjectsWithTag("collectable");
     }
 
 
@@ -26,5 +30,9 @@ public class ScoreManager : MonoBehaviour
     {
         score++;
         scoreText.text = score.ToString() + " POINTS";
+        
+        if (score == collectables.Length) {
+            SceneManager.LoadScene(2);
+        }
     }
 }
